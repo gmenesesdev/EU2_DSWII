aac07f2932e357a3e838e4c6c7a802f97dcb2e64
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-11-28 02:42:30 -0300

    first commit

---
+++ src/main/java/cl/ipss/evu2/Evu2Application.java
@@ -0,0 +1,13 @@
+package cl.ipss.evu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evu2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ipss/evu2/Evu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ipss.evu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evu2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
