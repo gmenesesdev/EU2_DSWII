525e05d211618be1862da8402175f19aea1e3d7e
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-11-30 22:38:26 -0300

    Se crea la vista de reservas-cliente.html y reservas-crear.html, ademas se modifica el controlador de reservas para que se pueda visualizar las reservas de un cliente y se pueda crear una reserva.

--- src/main/java/cl/ipss/evu2/controllers/ReservaController.java
+++ src/main/java/cl/ipss/evu2/controllers/ReservaController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -91,6 +92,13 @@ public class ReservaController {
 
         model.addAttribute("mensaje", "Reserva confirmada con éxito.");
         return "reserva-confirmada";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarReserva(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        reservaService.cancelarReserva(id);;
+        redirectAttributes.addFlashAttribute("msg", "Reserva cancelada con éxito");
+        return "redirect:/reservas/cliente";
     }
 
 }
